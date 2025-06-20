f21e085f0c6dc1ae858e95ca19aa758994d0dbdf
Abigail-Diaz <abigailsolano128@gmail.com>
2025-06-19 18:29:45 -0700

    refactor: Created a card to hold repeated code in the CurrentMonthIncome and BalanceSummary components - Moved logic to manage calculcations of total income and balance to App.jsx - Created a card styling module - Updated DashboardPage to use Card component with appropiate props

---
+++ src/Card.jsx
@@ -0,0 +1,14 @@
+// This component renders a card with a title and content.
+
+import styles from './card.module.css';
+
+function Card({ title, amount }) {
+    return (
+        <div className={styles.incomeCard}>
+            <h3> {title} </h3>
+            <p>${amount.toFixed(2)}</p>
+        </div>
+    );
+}
+
+export default Card;
--- src/Pages/DashboardPage.jsx
+++ src/Pages/DashboardPage.jsx
@@ -1,16 +1,25 @@
 import Navigation from '../Navigation.jsx';
 import Header from '../Header.jsx';
 import MonthlyExpenseGraph from '../MonthlyExpenseGraph.jsx';
+import Card from '.././Card.jsx';
+import RecentExpensesList from '../RecentExpensesList.jsx';
+import Footer from '../Footer.jsx'
+import styles from './Dashboard.module.css';
 
-function Dashboard({ header, transactions }) {
-  console.log("Dashboard component rendered");
+function Dashboard({ header, transactions, incomeTotal, balance, expensesByCategory }) {
   return (
     <>
       <Header header={header} />
       <Navigation />
-      <div style={{ justifyContent: 'center', margin: '1rem' }}>
+      <div >
         <MonthlyExpenseGraph transactions={transactions} />
+        <div className={styles.cardRow}>
+          <Card title = {'Total Income '} amount={incomeTotal} />
+          <Card title = {'Balance'} amount = {balance} />
+          <RecentExpensesList expensesByCategory={expensesByCategory} />
+        </div>
       </div>
+      <Footer />
     </>
   );
 }
