bfd89d88355719921e396ff48cc4978264e494d7
Abigail-Diaz <abigailsolano128@gmail.com>
2025-06-23 16:41:45 -0700

    Feat: Added loading state to DashboardPage by using reusable Loading component

--- src/Pages/DashboardPage.jsx
+++ src/Pages/DashboardPage.jsx
@@ -1,16 +1,20 @@
 import Navigation from '../Navigation.jsx';
 import Header from '../Header.jsx';
 import MonthlyExpenseGraph from '../MonthlyExpenseGraph.jsx';
-import Card from '.././Card.jsx';
+import Card from '../Card.jsx';
 import RecentExpensesList from '../RecentExpensesList.jsx';
-import Footer from '../Footer.jsx'
+import Footer from '../Footer.jsx';
+import Loading from '../Loading.jsx';
 import styles from './Dashboard.module.css';
 
-function Dashboard({ transactions, incomeTotal, balance, expensesByCategory, monthOptions }) {
+function Dashboard({ transactions, incomeTotal, balance, expensesByCategory, monthOptions, isLoading }) {
   const currentMonth = 'June 2025';
+
+  if (isLoading) return <Loading message="Loading Dashboard ..." />;
+
   return (
     <>
-      <div className = {styles.wrapper}>
+      <div className={styles.wrapper}>
         <div className={styles.cardColumn}>
           <Card title={'Total Income'} amount={incomeTotal} />
           <Card title={'Remaining'} amount={balance} />
