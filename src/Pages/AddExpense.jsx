9d5e0943b8ece1f863529e04eba8022da66da28f
Abigail-Diaz <abigailsolano128@gmail.com>
2025-06-24 11:28:52 -0700

    feat: Added a breadcrumb to allow users to go back to the expenses page

--- src/Pages/AddExpense.jsx
+++ src/Pages/AddExpense.jsx
@@ -1,23 +1,54 @@
 // Page to allow user to add a new expense to the transaction list
+import { useNavigate } from 'react-router-dom';
+import { FaArrowLeft } from 'react-icons/fa';
 import AddExpenseForm from "../AddExpenseForm";
 import Loading from "../Loading";
+import Error from "../Error";
 
-function AddExpense({handleAddExpense, categoryNames, isLoading, isError}) {
-    if (isLoading) return <Loading message = 'Loading expense form ... '/>
-    if (isError.state)
-    return (
-      <div style={{ marginTop: "200px", justifyContent: "center" }}>
-        <Error message={isError.errorMessage} />
-      </div>
-    );
+function AddExpense({ handleAddExpense, categoryNames, isLoading, isError }) {
+    const navigate = useNavigate();
+
+    if (isLoading) return <Loading message="Loading expense form ..." />;
+
+    if (isError.state) {
+        return (
+            <div style={{ marginTop: "200px", justifyContent: "center" }}>
+                <Error message={isError.errorMessage} />
+            </div>
+        );
+    }
+
     return (
         <>
-            <div style = {{marginLeft: '35%'}}>
-                <AddExpenseForm handleAddExpense={handleAddExpense}
-                    categoryNames={categoryNames}/>
+            <div style={{ marginLeft: '35%' }}>
+                {/* Breadcrumb-style Back Link */}
+                <div
+                    onClick={() => navigate('/transactions')}
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        marginTop: '100px',
+                        gap: '0.5rem',
+                        color: '#210629',
+                        cursor: 'pointer',
+                        fontWeight: 'bold',
+                        fontSize: '1rem',
+                    }}
+                >
+                    <FaArrowLeft />
+                    <span>Back to Expenses</span>
+                </div>
+
+                {/* Add Expense Form */}
+                <div>
+                    <AddExpenseForm
+                        handleAddExpense={handleAddExpense}
+                        categoryNames={categoryNames}
+                    />
+                </div>
             </div>
         </>
-    )
+    );
 }
 
 export default AddExpense;
