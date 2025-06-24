60b7fb841a313e6708d0a3283644fae5ebe66bdd
Abigail-Diaz <abigailsolano128@gmail.com>
2025-06-24 12:17:28 -0700

    feat: Implemented error handling

--- src/Pages/Transactions.jsx
+++ src/Pages/Transactions.jsx
@@ -1,17 +1,23 @@
 
 import TransactionList from '../TransactionList.jsx';
 import Loading from '../Loading.jsx';
+import Error from '../Error.jsx';
 
-function Transactions({ transactions, handleEditExpense, categoryNames, isLoading }) {
-  
-  if (isLoading) return <Loading message = "Loading Transactions..."/>
-  
+function Transactions({ transactions, handleEditExpense, categoryNames, isLoading, isError }) {
+
+  if (isLoading) return <Loading message="Loading Expenses ..." />
+  if (isError.state)
+    return (
+      <div style={{ marginTop: "200px", justifyContent: "center" }}>
+        <Error message={isError.errorMessage} />
+      </div>
+    );
+
   return (
     <>
       <div>
-        <TransactionList transactions={transactions} handleEditExpense={handleEditExpense} categoryNames = {categoryNames}/>
+        <TransactionList transactions={transactions} handleEditExpense={handleEditExpense} categoryNames={categoryNames} />
       </div>
-
     </>
   );
 }
