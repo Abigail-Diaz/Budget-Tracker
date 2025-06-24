bc171439131e98461a233c27bd0b9cbbbf3444be
Abigail-Diaz <abigailsolano128@gmail.com>
2025-06-24 12:31:29 -0700

    Refactor: Added dynamic month label fetching

---
+++ src/Pages/Dashboard.jsx
@@ -0,0 +1,53 @@
+import MonthlyExpenseGraph from "../MonthlyExpenseGraph.jsx";
+import Card from "../Card.jsx";
+import RecentExpensesList from "../RecentExpensesList.jsx";
+import Footer from "../Footer.jsx";
+import Loading from "../Loading.jsx";
+import styles from "./Dashboard.module.css";
+import Error from "../Error.jsx";
+
+import { format } from 'date-fns';
+
+function Dashboard({
+  transactions,
+  incomeTotal,
+  balance,
+  expensesByCategory,
+  monthOptions,
+  isLoading,
+  isError,
+}) {
+  const now = new Date();
+  const currentMonthLabel = format(now, 'MMMM yyyy');
+
+  if (isLoading) return <Loading message="Loading Dashboard ..." />;
+  if (isError.state)
+    return (
+      <div style={{ marginTop: "200px", justifyContent: "center" }}>
+        <Error message={isError.errorMessage} />
+      </div>
+    );
+
+  return (
+    <>
+      <div className={styles.wrapper}>
+        <div className={styles.cardColumn}>
+          <Card title={"Total Income"} amount={incomeTotal} />
+          <Card title={"Remaining"} amount={balance} />
+        </div>
+        <div className={styles.chartArea}>
+          <MonthlyExpenseGraph
+            transactions={transactions}
+            monthOptions={monthOptions}
+          />
+        </div>
+      </div>
+      <RecentExpensesList
+        expensesByCategory={expensesByCategory[currentMonthLabel]}
+      />
+      <Footer />
+    </>
+  );
+}
+
+export default Dashboard;
