35a7623f51f6fc402156fa1b92a0f568e70a7ee2
Abigail-Diaz <abigailsolano128@gmail.com>
2025-06-24 12:19:34 -0700

    refactor: Updated and structured the Navigation component - Formatted the code - Deleted header prop since it will stay on the App.jsx instead - Added conditional rendering of active links underline

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,13 +1,37 @@
-import styles from './Navigation.module.css';
-import { NavLink } from 'react-router-dom';
-function Navigation({setHeader}) {
-    return (<div className={styles.navBackground}>
-        <nav className={styles.nav}>
-            <NavLink to="/" onClick={() => setHeader('Dashboard')} >Dashboard</NavLink>
-            <NavLink to="/transactions" onClick={() => setHeader('Transactions')}>Transactions</NavLink>
-            <NavLink to="/budget" onClick={() => setHeader('Monthly Budget')}>Monthly Budget</NavLink>
-        </nav>
-    </div>)
+import styles from "./Navigation.module.css";
+import { NavLink } from "react-router-dom";
+
+function Navigation() {
+  return (
+    <div className={styles.navBackground}>
+      <nav className={styles.nav}>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? `${styles.navLink} ${styles.active}` : styles.navLink
+          }
+        >
+          Dashboard
+        </NavLink>
+        <NavLink
+          to="/transactions"
+          className={({ isActive }) =>
+            isActive ? `${styles.navLink} ${styles.active}` : styles.navLink
+          }
+        >
+          Expenses
+        </NavLink>
+        <NavLink
+          to="/budget"
+          className={({ isActive }) =>
+            isActive ? `${styles.navLink} ${styles.active}` : styles.navLink
+          }
+        >
+          Monthly Budget
+        </NavLink>
+      </nav>
+    </div>
+  );
 }
 
 export default Navigation;
