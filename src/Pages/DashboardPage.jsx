01a9def54cf00fcde80b8f97487508fa9afd0916
Abigail-Diaz <abigailsolano128@gmail.com>
2025-06-23 18:46:53 -0700

    chore: Format code

--- src/Pages/DashboardPage.jsx
+++ src/Pages/DashboardPage.jsx
@@ -1,29 +1,47 @@
-import Navigation from '../Navigation.jsx';
-import Header from '../Header.jsx';
-import MonthlyExpenseGraph from '../MonthlyExpenseGraph.jsx';
-import Card from '../Card.jsx';
-import RecentExpensesList from '../RecentExpensesList.jsx';
-import Footer from '../Footer.jsx';
-import Loading from '../Loading.jsx';
-import styles from './Dashboard.module.css';
+import MonthlyExpenseGraph from "../MonthlyExpenseGraph.jsx";
+import Card from "../Card.jsx";
+import RecentExpensesList from "../RecentExpensesList.jsx";
+import Footer from "../Footer.jsx";
+import Loading from "../Loading.jsx";
+import styles from "./Dashboard.module.css";
+import Error from "../Error.jsx";
 
-function Dashboard({ transactions, incomeTotal, balance, expensesByCategory, monthOptions, isLoading }) {
-  const currentMonth = 'June 2025';
+function Dashboard({
+  transactions,
+  incomeTotal,
+  balance,
+  expensesByCategory,
+  monthOptions,
+  isLoading,
+  isError,
+}) {
+  const currentMonth = "June 2025";
 
   if (isLoading) return <Loading message="Loading Dashboard ..." />;
+  if (isError.state)
+    return (
+      <div style={{ marginTop: "200px", justifyContent: "center" }}>
+        <Error message={isError.errorMessage} error={isError.error} />
+      </div>
+    );
 
   return (
     <>
       <div className={styles.wrapper}>
         <div className={styles.cardColumn}>
-          <Card title={'Total Income'} amount={incomeTotal} />
-          <Card title={'Remaining'} amount={balance} />
+          <Card title={"Total Income"} amount={incomeTotal} />
+          <Card title={"Remaining"} amount={balance} />
         </div>
         <div className={styles.chartArea}>
-          <MonthlyExpenseGraph transactions={transactions} monthOptions={monthOptions} />
+          <MonthlyExpenseGraph
+            transactions={transactions}
+            monthOptions={monthOptions}
+          />
         </div>
       </div>
-      <RecentExpensesList expensesByCategory={expensesByCategory[currentMonth]} />
+      <RecentExpensesList
+        expensesByCategory={expensesByCategory[currentMonth]}
+      />
       <Footer />
     </>
   );
